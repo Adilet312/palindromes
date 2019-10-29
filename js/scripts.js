e69bc7904cff2108e240686d00826240f4df25e9
Adilet Momunaliev, Nina Potrebich and Wei Dai <wei@>
2019-10-29 15:06:39 -0700

    palindrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+  $("#form1").submit(function(event) {
+    event.preventDefault();
 
+    var string = $("#checkString").val();
+
+    var result = palindrome(string);
+
+    $("#resultBox").text(result);
+
+    function palindrome(str) {
+      var len = str.length;
+      var med = Math.floor(len/2);
+      var j = len-1;
+      for (var i = 0; i < med; i++) {
+        if (str[i] != str[j]) {
+          return false;
+        }
+        j--;
+      }
+      return true;
+    };
+
+  });
 
 
 });
